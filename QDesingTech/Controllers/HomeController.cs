0ef86b1b36f759ac5bc222ad3e3bbbc73437ef27
fix <barbarosproject@gmail.com>
2022-03-17 09:27:46 +0300

    git changes control

--- QDesingTech/Controllers/HomeController.cs
+++ QDesingTech/Controllers/HomeController.cs
@@ -17,49 +17,43 @@ namespace QDesingTech.Controllers
         public ActionResult Index()
         {
 
-
-
             return View();
         }
 
         public ActionResult About()
         {
-           
+
             if (Request.Form.Count > 0)
             {
 
                 string ssid = Request.Form["ssid"];
                 string pass = Request.Form["pass"];
                 string wpa = Request.Form["wpa"];
-                
+
 
                 string wifi = "WIFI:S:" + ssid + ";T:" + wpa + ";P:" + pass + ";;";
-              
+
 
                 QRCodeEncoder cnv = new QRCodeEncoder();
 
                 //encoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
                 //encoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.H;
-               
+
 
                 cnv.QRCodeScale = 5;
                 cnv.QRCodeVersion = 3;
-                cnv.QRCodeBackgroundColor =Color.White;
+                cnv.QRCodeBackgroundColor = Color.White;
                 cnv.QRCodeForegroundColor = Color.Black;
 
                 Bitmap bmp = cnv.Encode(wifi);
 
-
                 //bmp.Save("C:\\FiX\\MVC\\QRGenerator\\QR.jpeg", ImageFormat.Jpeg);
-
 
                 Response.ContentType = "image/jpeg";
                 bmp.Save(Response.OutputStream, ImageFormat.Jpeg);
                 bmp.Dispose();
                 Response.Flush();
 
-
-                
             }
 
             return View();
@@ -74,14 +68,12 @@ namespace QDesingTech.Controllers
 
                 string text1 = Request.Form["text"];
                 //string text2 = "https://" + "docs.google.com/" + "document/d/" + text1 +"/edit?usp=sharing";
-               // string zz = "URLTO:" + text1;
+                // string zz = "URLTO:" + text1;
 
-                   //docs.google.com/document/d/1ivfjjb0-YyMgl63bl7Ev99TFBvAyGqCHggdwH6542D8/edit?usp=sharing
+                //docs.google.com/document/d/1ivfjjb0-YyMgl63bl7Ev99TFBvAyGqCHggdwH6542D8/edit?usp=sharing
                 QRCodeEncoder karekod = new QRCodeEncoder();
 
                 //QRCodeDecoder dc = new QRCodeDecoder();
-
-
 
                 //Barcode[] barcodes = BarcodeReader.ReadBarcodes(settings, bmp);
                 karekod.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.L;
@@ -98,12 +90,10 @@ namespace QDesingTech.Controllers
                 Response.ContentType = "image/jpeg";
                 ViewBag.karekodresim = resim;
 
-         
-
                 //System.Drawing.Image im = cnv.Encode(text1);
 
                 ////bmp.Dispose();      
-                
+
                 //Response.Flush();
             }
 
@@ -112,27 +102,16 @@ namespace QDesingTech.Controllers
 
         public ActionResult ABC()
         {
-
-
-
-
             return View();
-
         }
 
         public ActionResult XX()
         {
-
-
             return View();
         }
 
         public ActionResult ABCD()
         {
-
-
-
-
             return View();
 
         }
